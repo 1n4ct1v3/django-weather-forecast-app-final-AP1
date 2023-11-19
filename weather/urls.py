f3652e1f7797fce297fa1b437d86293c239e824a
inactive <mirasb7292@gmail.com>
2023-11-19 18:54:44 +0600

    beginning of git con

---
+++ weather/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path, include
+from . import views
+
+urlpatterns = [
+    path('', views.index, name="home"),
+    path('about/', views.about, name="about"),
+    path('contact/', views.contact, name="contact"),
+    path('delete/<city_name>/', views.delete_city,name="delete_city"),
+]
