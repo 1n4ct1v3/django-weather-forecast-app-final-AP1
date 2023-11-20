200d5b563e14b026b73badfd505c09cc82641d0d
inactive <mirasb7292@gmail.com>
2023-11-20 20:03:00 +0600

    fixed somehow

--- weatherapp/settings.py
+++ weatherapp/settings.py
@@ -71,18 +71,15 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'weatherapp.wsgi.application'
 
+DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': "postgres",
-        'USER': "postgres",
-        'PASSWORD': "miras",
-        'HOST': "localhost",
-        'PORT': "5432"
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
